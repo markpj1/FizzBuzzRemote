153e02ce5bceed5c065fa54f91571a5489042db5
markpj1 <markpjaramillo@gmail.com>
2015-04-08 15:41:31 -0700

    if/else statement exercise

--- ifElse.js
+++ ifElse.js
@@ -23,3 +23,43 @@ function translator (lang) {
 translator("es");
 translator("de");
 translator("en");
+
+// Grade Assigner takes 1 arg, returnes grade A-f 
+function gradeAssigner(score) {
+	if(score >= 90) {
+		console.log("You got a: A");
+	}
+	else if(score >= 80) {
+		console.log("You got a: B")
+	}
+	else if(score >= 70) {
+		console.log("You got a: C")
+	}
+	else if(score >= 65) {
+		console.log("You got a: D")
+	}
+	else {
+		console.log("Not Yet")
+	};
+
+};
+gradeAssigner(90);
+gradeAssigner(85);
+gradeAssigner(75);
+gradeAssigner(64);
+gradeAssigner(45);
+
+//function name plurizer add s to pair or more unless geese or sheep
+
+function plurize(noun, num) {
+	if(noun !== "sheep" && noun !== "geese" && num !== 1) {
+		console.log(num + ": " + noun + "s.");
+	}
+	else {
+		console.log(num + ": " + noun);
+	}
+};
+plurize("cat", 1);
+plurize("dog", 2);
+plurize("sheep", 4);
+plurize("geese", 1);
